67895c3318ed3238a24a38d8f509124cdcadfe2f
Rachanonnn <rachanonnn2004@gmail.com>
2024-05-26 01:45:38 +0700

    fix: FooterSection add path

--- src/app/components/FooterSection.tsx
+++ src/app/components/FooterSection.tsx
@@ -10,15 +10,21 @@ const FooterSection: React.FC = () => {
       <div className="flex flex-col justify-center items-center">
         <h2 className='text-white text-xl font-semibold relative justify-center items-center mt-10 mb-6'>Contact me</h2>
         <div className="flex justify-center items-center gap-2 mb-12">
-            <IconButton text="Instagram" color="bg-gradient-to-tr from-yellow-500 to-purple-500 via-pink-500">
-              <GrInstagram size={25}/>
-            </IconButton>
-            <IconButton text="Github">
-              <FaGithub size={25}/>
-            </IconButton>
-            <IconButton text="Linkedin" color="bg-blue-500">
-              <LiaLinkedin size={25}/>
-            </IconButton>
+            <a href='https://www.instagram.com/nackkyyss/' target='_blank'>
+              <IconButton text="Instagram" color="bg-gradient-to-tr from-yellow-500 to-purple-500 via-pink-500">
+                <GrInstagram size={25}/>
+                </IconButton>
+            </a>
+            <a href='https://github.com/Rachanonnn' target='_blank'>
+              <IconButton text="Github">
+                <FaGithub size={25}/>
+              </IconButton>
+            </a>
+            <a href='https://www.linkedin.com/in/rachanon-sanprasit-56a434303/' target='_blank'>
+              <IconButton text="Linkedin" color="bg-blue-500">
+                <LiaLinkedin size={25}/>
+              </IconButton>
+            </a>
         </div>
       </div>
     </section>
