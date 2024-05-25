7ad07360c065bd7aa2f9dd1f70f75cf7e3bcebea
Rachanonnn <rachanonnn2004@gmail.com>
2024-05-26 00:19:21 +0700

    fix: navbar cursor-pointer

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -9,7 +9,7 @@ const NavLink = ({ href, title} : {href: any, title: any}) => {
         smooth={true} 
         offset={-100} 
         duration={500}
-        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-lg rounded md:p-0 hover:text-white"
+        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-lg rounded md:p-0 hover:text-white cursor-pointer"
         >
             {title}
         </Link>
