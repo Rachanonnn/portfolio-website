637507e4f9873bb0389e9071152f8c77a29146e8
Rachanonnn <rachanonnn2004@gmail.com>
2024-05-26 02:02:29 +0700

    fix: ProfileSection

--- src/app/components/ProfileSection.tsx
+++ src/app/components/ProfileSection.tsx
@@ -16,7 +16,7 @@ const ProfileSection = () => {
             className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-                        Hi, I'm{" "}
+                        Hi, I&apos;m{" "}
                     </span>
                     <br></br>
                     <TypeAnimation
