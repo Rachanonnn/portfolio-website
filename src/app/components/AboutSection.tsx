cadaf60bf34c59bb1bf9da097693c8755d0eaf26
Rachanonnn <rachanonnn2004@gmail.com>
2024-06-14 01:27:19 +0700

    feat: more project, certificate

--- src/app/components/AboutSection.tsx
+++ src/app/components/AboutSection.tsx
@@ -1,144 +1,146 @@
-'use client'
-import React, { useTransition, useState } from 'react'
-import Image from "next/image"
-import TabButton from './TabButton'
-import { motion } from "framer-motion"
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+import { motion } from "framer-motion";
 
 const TAB_DATA = [
-    {
-        title : "Skills",
-        id: "skills",
-        content: (
-            <div className='flex flex-row justify-start'>
-                <div className='mr-auto'>
-                    <h2 className='text-xl font-semibold mb-3 justify-start'>Backend</h2>
-                    <ul className='list-disc pl-2'>
-                        <li>Go-Fiber</li>
-                        <li>RESTful APIS</li>
-                        <li>NodeJS</li>
-                        <li>ExpressJS</li>
-                        <li>MongoDB</li>
-                    </ul>
-                </div>
-                <div className='mx-auto'>
-                    <h2 className='text-xl font-semibold mb-3 justify-start'>Frontend</h2>
-                    <ul className='list-disc pl-2'>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>TypeScript</li>
-                        <li>TailwindCSS</li>
-                    </ul>
-                </div>
-                <div className='mx-auto'>
-                    <h2 className='text-xl font-semibold mb-3 justify-start'>Tools</h2>
-                    <ul className='list-disc pl-2'>
-                        <li>Postman</li>
-                        <li>Git</li>
-                        <li>3T Studio</li>
-                        <li>DBGate</li>
-                    </ul>
-                </div>
-            </div>
-        )
-    },
-    {
-        title : "Education",
-        id: "education",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li className='mb-1'>Bachelor Degree</li>
-                <p className='mb-4'> Computer Engineering and Digital Technology, Chulalongkorn University</p>
-                <li className='mb-1'>High School</li>
-                <p>Math-Sci (Gifted Program), Suankularb Wittayalai Thonburi School</p>
-            </ul>
-        )
-    },
-    {
-        title : "Certifications",
-        id: "certifications",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>Udemy Web Development Bootcamp 2024</li>
-            </ul>
-        )
-    },
-    {
-        title : "Experiences",
-        id: "experiences",
-        content: (
-            <ul className='list-disc pl-2'>
-                <li>2024 | Backend Developer Internship at iBotnoi</li>
-            </ul>
-        )
-    }
-]
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <div className="flex flex-row justify-start">
+        <div className="mr-auto">
+          <h2 className="text-xl font-semibold mb-3 justify-start">Backend</h2>
+          <ul className="list-disc pl-2">
+            <li>Go-Fiber</li>
+            <li>RESTful APIS</li>
+            <li>NodeJS</li>
+            <li>ExpressJS</li>
+            <li>MongoDB</li>
+          </ul>
+        </div>
+        <div className="mx-auto">
+          <h2 className="text-xl font-semibold mb-3 justify-start">Frontend</h2>
+          <ul className="list-disc pl-2">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+            <li>TailwindCSS</li>
+          </ul>
+        </div>
+        <div className="mx-auto">
+          <h2 className="text-xl font-semibold mb-3 justify-start">Tools</h2>
+          <ul className="list-disc pl-2">
+            <li>Postman</li>
+            <li>Git</li>
+            <li>3T Studio</li>
+            <li>DBGate</li>
+          </ul>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li className="mb-1">Bachelor Degree</li>
+        <p className="mb-4">
+          {" "}
+          Computer Engineering and Digital Technology, Chulalongkorn University
+        </p>
+        <li className="mb-1">High School</li>
+        <p>Math-Sci (Gifted Program), Suankularb Wittayalai Thonburi School</p>
+      </ul>
+    ),
+  },
+  {
+    title: "Experiences",
+    id: "experiences",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>2024 | Backend Developer Internship at iBotnoi</li>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [_, startTransition] = useTransition();
 
-  const handleTabChange = (id : any) => {
+  const handleTabChange = (id: any) => {
     startTransition(() => {
-        setTab(id);
+      setTab(id);
     });
   };
   const currentTab = TAB_DATA.find((t) => t.id === tab);
   return (
-    <section className='text-white' id='about'>
-      <div 
-      className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <motion.div
-        initial={{ opacity: 0, y: 50}} 
-        animate={{opacity: 1, y: 0}}
-        transition={{ duration: 0.4}} 
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
         >
-            <Image 
-            src={"/images/about-image.jpg"} 
-            alt='about-image' 
-            className='rounded-2xl'
-            width={500} 
-            height={500} />
+          <Image
+            src={"/images/about-image.jpg"}
+            alt="about-image"
+            className="rounded-2xl"
+            width={500}
+            height={500}
+          />
         </motion.div>
-        <motion.div 
-        initial={{ opacity: 0, y: 50}} 
-        animate={{opacity: 1, y: 0}}
-        transition={{ duration: 0.4}} 
-        className='mt-4 md:mt-0 text-left flex flex-col h-full'>
-            <h2 className='text-3xl font-bold text-white mt-4 mb-10'>About Me</h2>
-            <p className='text-base lg:text-lg'>
-                Hello! My name is Rachanon, and I am a computer engineering student
-                with a passion for technology and problem-solving. As someone deeply
-                interested in the intersection of cybersecurity, web development, and data
-                engineering, I am eager to contribute my skills and knowledge to innovative
-                projects. I pride myself on being a quick learner, always ready to explore new
-                concepts and adapt to dynamic environments. My passion for technology
-                drives me to stay up-to-date with industry trends and continuously improve
-                my abilities.
-            </p>
-            <div className='flex flex-row justify-start mt-8'>
-                <TabButton selectTab={() => handleTabChange("skills")} active={tab === "skills"}>
-                    {" "}
-                    Skills{" "}
-                </TabButton>
-                <TabButton selectTab={() => handleTabChange("education")} active={tab === "education"}>
-                    {" "}
-                    Education{" "}
-                </TabButton>
-                <TabButton selectTab={() => handleTabChange("certifications")} active={tab === "certifications"}>
-                    {" "}
-                    Certifications{" "}
-                </TabButton>
-                <TabButton selectTab={() => handleTabChange("experiences")} active={tab === "experiences"}>
-                    {" "}
-                    Experiences{" "}
-                </TabButton>
-            </div>
-            <div className='mt-8'>{currentTab ? currentTab.content : "Content not available"}</div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4 }}
+          className="mt-4 md:mt-0 text-left flex flex-col h-full"
+        >
+          <h2 className="text-3xl font-bold text-white mt-4 mb-10">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Hello! My name is Rachanon, and I am a computer engineering student
+            with a passion for technology and problem-solving. As someone deeply
+            interested in the intersection of cybersecurity, web development,
+            and data engineering, I am eager to contribute my skills and
+            knowledge to innovative projects. I pride myself on being a quick
+            learner, always ready to explore new concepts and adapt to dynamic
+            environments. My passion for technology drives me to stay up-to-date
+            with industry trends and continuously improve my abilities.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("experiences")}
+              active={tab === "experiences"}
+            >
+              {" "}
+              Experiences{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {currentTab ? currentTab.content : "Content not available"}
+          </div>
         </motion.div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- src/app/components/ProjectSection.tsx
+++ src/app/components/ProjectSection.tsx
@@ -1,86 +1,108 @@
-'use client'
-import React, { useState, useRef } from 'react'
-import ProjectCard from './ProjectCard'
-import ProjectTag from './ProjectTag'
-import { motion, useInView } from "framer-motion"
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
 
 const projectsData = [
-    {
-        id: 1,
-        title: "Gear Up",
-        description: "Gear Up is a car rental web application that allows users to create an account, rent cars from providers, and checkout while viewing their rental history. Users also have the option to register as providers, offering their cars for rent with detailed information.",
-        image: "/images/projects/1.jpg",
-        tag: ["All", "Web"],
-        gitURL: "https://github.com/2110503TACEDT66/cedt-se-project-borisat_yaeng_tua",
-        previewURL: "https://cedt-se-project-frontend.vercel.app/",
-    },
-    {
-        id: 1,
-        title: "Gear Up",
-        description: "Gear Up is a car rental web application that allows users to create an account, rent cars from providers, and checkout while viewing their rental history. Users also have the option to register as providers, offering their cars for rent with detailed information.",
-        image: "/images/projects/1.jpg",
-        tag: ["All"],
-        gitURL: "https://github.com/2110503TACEDT66/cedt-se-project-borisat_yaeng_tua",
-        previewURL: "https://cedt-se-project-frontend.vercel.app/",
-    }
-    ]
+  {
+    id: 1,
+    title: "Gear Up",
+    description:
+      "Gear Up is a car rental web application that allows users to create an account, rent cars from providers, and checkout while viewing their rental history. Users also have the option to register as providers, offering their cars for rent with detailed information.",
+    image: "/images/projects/1.jpg",
+    tag: ["All", "Web"],
+    gitURL:
+      "https://github.com/2110503TACEDT66/cedt-se-project-borisat_yaeng_tua",
+    previewURL: "https://cedt-se-project-frontend.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "Ecommerce (On-going)",
+    description:
+      "Our e-commerce project leverages Go Fiber for the backend and React/Next.js for the frontend. It's straightforward, enabling users to create accounts, add products to their carts, and complete purchases. Users can easily sign up using Google authentication and update their profiles as needed.",
+    image: "/images/projects/2.png",
+    tag: ["All", "Web"],
+    gitURL: "https://github.com/Rachanonnn/go-ecommerce",
+    previewURL: "",
+  },
+  {
+    id: 3,
+    title: "Udemy Web Development Bootcamp Certification",
+    description:
+      "The Complete Web Development Bootcamp, learn to code and become a full-stack web developer with the HIGHEST RATED courses in the history of Udemy. I have completed this course and I have learned a lot from it.",
+    image:
+      "https://media.discordapp.net/attachments/1240950010473091155/1240950372055384185/UC-9e61f8ba-bfdc-4ce7-a93c-7652f12b69ef.jpg?ex=666c0518&is=666ab398&hm=bbcad32600bde8b263d5053875a383b3a792bd7e8ba74cfac2ec19642eac957a&=&format=webp&width=828&height=616",
+    tag: ["All", "Certificate"],
+    gitURL:
+      "https://www.udemy.com/certificate/UC-9e61f8ba-bfdc-4ce7-a93c-7652f12b69ef/",
+    previewURL:
+      "https://udemy-certificate.s3.amazonaws.com/image/UC-9e61f8ba-bfdc-4ce7-a93c-7652f12b69ef.jpg?v=1715932665000",
+  },
+];
 
 const ProjectSection = () => {
-
   const [tag, setTag] = useState("All");
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
-  const handleTabChange = (newTag : string) => {
+  const handleTabChange = (newTag: string) => {
     setTag(newTag);
   };
 
-  const filteredProjects = projectsData.filter((project) => 
+  const filteredProjects = projectsData.filter((project) =>
     project.tag.includes(tag)
   );
 
   const cardVariants = {
     initial: { y: 50, opacity: 0 },
-    animate : { y: 0, opacity: 1 },
+    animate: { y: 0, opacity: 1 },
   };
 
   return (
-    <section id='projects'>
-      <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8'>My Projects</h2>
-      <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
-        <ProjectTag 
-            onClick={handleTabChange} 
-            name="All" 
-            isSelected={tag === "All"} 
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8">
+        My Activities
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTabChange}
+          name="All"
+          isSelected={tag === "All"}
         />
-        <ProjectTag 
-            onClick={handleTabChange} 
-            name="Web" 
-            isSelected={tag === "Web"} 
+        <ProjectTag
+          onClick={handleTabChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTabChange}
+          name="Certificate"
+          isSelected={tag === "Certificate"}
         />
       </div>
-      <ul ref={ref} className='grid md:grid-cols-3 gap-8 md:gap-12'>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
         {filteredProjects.map((project, index) => (
-            <motion.li
+          <motion.li
             key={index}
             variants={cardVariants}
             initial="initial"
             animate={isInView ? "animate" : "initial"}
             transition={{ duration: 0.3, delay: 0.2 * index }}
-            >
+          >
             <ProjectCard
-                key={project.id}
-                title={project.title}
-                description={project.description}
-                imgURL={project.image}
-                gitURL={project.gitURL}
-                previewURL={project.previewURL}
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgURL={project.image}
+              gitURL={project.gitURL}
+              previewURL={project.previewURL}
             />
-            </motion.li>
+          </motion.li>
         ))}
-        </ul>
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
