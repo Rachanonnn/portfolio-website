bf585774b08549bbebfb544e2e33f2ac3738bc48
Rachanonnn <rachanonnn2004@gmail.com>
2024-06-23 22:38:24 +0700

    chore: update cv

--- src/app/components/ProfileSection.tsx
+++ src/app/components/ProfileSection.tsx
@@ -1,71 +1,78 @@
-'use client'
+"use client";
 import React from "react";
-import Image from "next/image"
+import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 
 const ProfileSection = () => {
-    return (
-        <section className="my-5" id="home">
-        <div className="grid grid-cols-1 sm:grid-cols-12">
-            <motion.div 
-            initial={{ opacity: 0, scale: 0.5}} 
-            animate={{opacity: 1, scale: 1}}
-            transition={{ duration: 0.5}} 
-            className="col-span-7 place-self-center text-center sm:text-left">
-                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-                        Hi, I&apos;m{" "}
-                    </span>
-                    <br></br>
-                    <TypeAnimation
-                        sequence={[
-                            'Nack',1000,
-                            'Web Developer',1000
-                        ]}
-                        wrapper="span"
-                        speed={50}
-                        repeat={Infinity}
-                    />
-                </h1>
-                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl ">
-                    I am a computer engineering student with a passion for web development and software development. I am always looking for new and exciting projects to work on.
-                </p>
-                <div>
-                    <Link 
-                    to="footer"
-                    spy={true} 
-                    smooth={true} 
-                    offset={-100} 
-                    duration={500}>
-                    <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white cursor-pointer">
-                    Contact me
-                    </button>
-                    </Link>
-                    <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
-                        <a href="https://drive.google.com/file/d/1eO_g3Kb5fsD4G3Y0qk_9VOiTwlNM58tp/view?usp=sharing" className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">Download CV</a>
-                    </button>
-                </div>
-            </motion.div>
-            <motion.div
-            initial={{ opacity: 0, scale: 0.5}} 
-            animate={{opacity: 1, scale: 1}}
-            transition={{ duration: 0.5}}  
-            className="col-span-5 place-self-center mt-4 lg:mt-0">
-                <div className="rounded-full bg-[#181818] w-[270px] h-[270px] lg:w-[400px] lg:h-[400px] relative mt-5">
-                    <Image
-                        src="/images/boy-using-laptop.png"
-                        alt="boyusinglaptopimage"
-                        className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                        width={300}
-                        height={300}
-                    />
-                </div>
-            </motion.div>
-        </div>
-        </section>
-    )
-}
+  return (
+    <section className="my-5" id="home">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-7 place-self-center text-center sm:text-left"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              Hi, I&apos;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={["Nack", 1000, "Web Developer", 1000]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl ">
+            I am a computer engineering student with a passion for web
+            development and software development. I am always looking for new
+            and exciting projects to work on.
+          </p>
+          <div>
+            <Link
+              to="footer"
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white cursor-pointer">
+                Contact me
+              </button>
+            </Link>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3">
+              <a
+                href="https://drive.google.com/file/d/1qekaTbHq_p3jIVY6tXFxndZYbB0e1Zt8/view"
+                className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2"
+              >
+                Download CV
+              </a>
+            </button>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-5 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[270px] h-[270px] lg:w-[400px] lg:h-[400px] relative mt-5">
+            <Image
+              src="/images/boy-using-laptop.png"
+              alt="boyusinglaptopimage"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
 
 export default ProfileSection;
